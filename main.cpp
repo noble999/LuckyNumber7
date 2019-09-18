e08ad886e8af546a448bc19e804585a1ef5a0cfc
noble999 <nleduc2@mail.csuchico.edu>
2019-09-18 16:13:12 -0700

    new main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author:Noble LeDuc
 #include <iostream>
 
 using namespace std;
@@ -10,15 +10,20 @@ int main()
   cout<<"Guess a number between 1-10: ";
   cin>>guess;
 
-
+  if(guess==7)
+  {
   //when they guess correctly (7), display:
   cout<<"YES! Lucky number 7!\n";
-
+  }
   //when they guess just off by one (either 6 or 8), display:
+  else if((guess > 7 && guess < 9)|| (guess>5 && guess<7))
+  {
   cout<<"Oh, so close!\n";
-
+  }
   //when they guess any other number, display:
+  else
+  {
   cout<<"Waaay off, sorry.\n";
-
+  }
   return 0;
 }
